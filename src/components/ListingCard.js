265972bd87642f4fae2094c1ed80a5e07270dbe0
alliesoldau <86414056+alliesoldau@users.noreply.github.com>
2022-12-01 10:48:05 -0500

    Update ListingCard.js

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,25 +1,54 @@
-import React from "react";
-import ListingCard from "./ListingCard";
+import React, { useState } from "react";
 
-function ListingsContainer({ listingData, deleteFromList }) {
+function ListingCard({ listing, deleteFromList }) {
 
-  const listingMapping = listingData.map((listing) => {
-    return(
-      <ListingCard
-        key={listing.id}
-        listing={listing}
-        deleteFromList={deleteFromList}
-      />
-  )
-  })
+  const [toggle, setToggle] = useState(false)
 
+  function handleButtonToggle() {
+    setToggle((toggle) => !toggle)
+  }
+
+  function handleDelete() {
+    fetch(`http://localhost:6001/listings/${listing.id}`, {
+      method: "DELETE",
+    })
+    .then((r)=>r.json())
+    .then(deleteFromList(listing))
+  }
+  
   return (
-    <main>
-      <ul className="cards">
-        {listingMapping}
-      </ul>
-    </main>
+    <li className="card">
+      <div className="image">
+        <span className="price">$0</span>
+        <img src={listing.image} alt={listing.description} />
+      </div>
+      <div className="details">
+        {toggle ? (
+          <button 
+            className="emoji-button favorite active"
+            onClick={handleButtonToggle}
+            >
+              ★
+            </button>
+        ) : (
+          <button 
+            className="emoji-button favorite"
+            onClick={handleButtonToggle}
+            >
+              ☆
+            </button>
+        )}
+        <strong>{listing.description}</strong>
+        <span> · {listing.location}</span>
+        <button 
+          className="emoji-button delete"
+          onClick={handleDelete}
+          >
+            🗑
+          </button>
+      </div>
+    </li>
   );
 }
 
-export default ListingsContainer;
+export default ListingCard;
