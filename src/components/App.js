aceb520ad89c6be2afe65108647520b3b98f2709
alliesoldau <86414056+alliesoldau@users.noreply.github.com>
2022-12-01 10:45:29 -0500

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+    .then((r) => r.json())
+    .then((data) => setListingData(data))
+  }, [])
+
+  const [listingData, setListingData] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
+
+  function deleteFromList(listingRIP) {
+    const updatedListings = listingData
+    .filter((listing) => listing.id !== listingRIP.id)
+    setListingData(updatedListings)
+  }
+
+    const filteredListings = listingData
+    .filter((listing) => listing.description.toLowerCase().includes(searchTerm.toLowerCase()))
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header 
+        handleSearch={setSearchTerm}
+      />
+      <ListingsContainer 
+      listingData={filteredListings}
+      deleteFromList={deleteFromList}
+      />
     </div>
   );
 }
